9442a853eda83221a9cae304bbdff9375fbc9d9c
unknown <btkkentxx2002@gmail.com>
2022-07-26 20:55:19 +0700

    jv

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-    public  static boolean checkPrime(int j){
-        if (j < 2 ){
+    public static boolean checkPrime(int j) {
+        if (j < 2) {
             return false;
-        }for (int i = 2 ; i <= Math.sqrt(j);i++){
-            if (j % i ==0){
+        }
+        for (int i = 2; i <= Math.sqrt(j); i++) {
+            if (j % i == 0) {
                 return false;
             }
         }
-        return  true;
+        return true;
     }
 
+    //-----//
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
         System.out.println("Nhập số");
         int number = scanner.nextInt();
-        int j = 2 ;
-        while (j < number){
-            if (checkPrime(j)){
+        int j = 2;
+        while (j < number) {
+            if (checkPrime(j)) {
                 System.out.println(j);
             }
-//            if (j >= 100){
-//                break;
-//            }
             j++;
         }
-
-
     }
 }
